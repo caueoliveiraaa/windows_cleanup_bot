60460983a3cb3bf6e54d23a5512f5f37c253720a
caueoliveiraaa <leo.siscobra@gmail.com>
2023-10-22 19:11:40 -0300

    Data Types

--- 01_python_basics/02_data_types.py
+++ 01_python_basics/02_data_types.py
@@ -35,34 +35,68 @@ x = 1j
 print(x)
 print(type(x)) 
 
+
 # list
+x = ["apple", "banana", "cherry"]
+print(x)
+print(type(x)) 
 
 
 # tuple
+x = ("apple", "banana", "cherry")
+print(x)
+print(type(x)) 
 
 
 # range
+x = range(6)
+print(x)
+print(type(x)) 
 
 
 # dictionary 
+x = {"name" : "John", "age" : 36}
+print(x)
+print(type(x)) 
 
 
 # set
+x = {"apple", "banana", "cherry"}
+print(x)
+print(type(x)) 
 
 
 # frozenset
+x = frozenset({"apple", "banana", "cherry"})
+print(x)
+print(type(x)) 
 
 
 # Boolean
+x = True
+print(x)
+print(type(x)) 
 
 
 # bytes
+x = b"Hello"
+print(x)
+print(type(x)) 
 
 
 # bytearray
+x = bytearray(5)
+print(x)
+print(type(x)) 
 
 
 # memoryview
+x = memoryview(bytes(5))
+print(x)
+print(type(x)) 
 
 
 # NoneType
+x = None
+print(x)
+print(type(x)) 
