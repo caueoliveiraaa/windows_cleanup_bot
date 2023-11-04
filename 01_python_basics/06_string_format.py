d17ea4383f57e7a7dc79068e8dd52491db13b6fe
caueoliveiraaa <leo.siscobra@gmail.com>
2023-11-04 16:58:42 -0300

    strings

--- 01_python_basics/06_string_format.py
+++ 01_python_basics/06_string_format.py
@@ -2,7 +2,6 @@
 # The format() method takes the passed arguments, formats them,
 # and places them  in the string where the placeholders {} are.
 
-
 age = 36
 text = "My name is Smith, and I am {}"
 print(text.format(age))
@@ -14,11 +13,9 @@ zipcode = 51
 social_seurity = 18715158156
 txt = 'Phone number: {}, zip code: {}, social security number: {}.'
 print(txt.format(phonenumber, zipcode, social_seurity))
-
 # You can indicate where the variables go with indexes
 txt = 'Phone number: {0}, zip code: {1}, social security number: {2}.'
 print(txt.format(phonenumber, zipcode, social_seurity))
 # And you can change the positions as you will
 txt = 'Phone number: {2}, zip code: {1}, social security number: {0}.'
 print(txt.format(phonenumber, zipcode, social_seurity))
-
