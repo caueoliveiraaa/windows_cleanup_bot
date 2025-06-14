7041e3ef082280eca9136eb5d6834917f0ef8c6a
caueoliveiraaa <77469726+caueoliveiraaa@users.noreply.github.com>
2025-06-14 15:24:08 -0300

    Add better documentation to the project

--- clean_up_bot.py
+++ clean_up_bot.py
@@ -1,9 +1,11 @@
-from command_list import command_list
-import sys
+""" This bot is responsable for cleaning up space in the computer """
 import os
+import sys
+from command_list import command_list
 
 
 def display_error() -> None:
+    """ Displays the error information """
     exctp, exc, exctb = sys.exc_info()
     message = f'\ntraceback:{exctb.tb_frame.f_code.co_name}'
     message += f':{exctb.tb_lineno}:{exctp}:'
@@ -12,12 +14,14 @@ def display_error() -> None:
 
 
 def main() -> None:
+    """ Executes the bot """
     for command in command_list:
         print(('-' * 120) + f'\nExecuting command: {command}\n')
         try:
             os.system(command)
-        except Exception:
+        except Exception as erro:
             display_error()
+            raise erro
 
 
 if __name__ == '__main__':
